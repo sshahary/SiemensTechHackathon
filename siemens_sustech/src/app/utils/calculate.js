e0394e3ca7ceb3f7e885be82b3a231a9e4de95f6
Rajkumar Pambhar <rajkumarpambhar770@gmail.com>
2024-03-12 04:20:13 +0100

    Created Formula 1:

--- siemens_sustech/src/app/utils/calculate.js
+++ siemens_sustech/src/app/utils/calculate.js
@@ -1,14 +1,31 @@
+const processing = ({
+  volume,
+  quantity,
+  material,
+  process,
+  maxPrintingSpeed,
+  volumeOfMachine,
+  powerStandardOfMachine,
+  processingTime,
+  countryEnergyMix,
+  recycledPortion,
+  emissionFactor
+}) => {
+  let massOfPart = 0;
+  let processingTime = 0;
+  let buyToFlyRatio = 0;
+  let CO2eqForProcessing = 0;
+  let CO2eqForWaste = 0;
 
-
-const processing = ({ volume, quantity, material, process, maxPrintingSpeed}) => {
-    let processingTime = 0;
-    let buyToFlyRatio = 0;
-    let CO2eqForProcessing = 0;
-    let CO2eqForWaste = 0;
-
-    processingTime = volume * quantity / maxPrintingSpeed;
-    buyToFlyRatio = 
-
-    let result = CO2eqForProcessing + CO2eqForWaste;
-    return (result);
+  processingTime = (volume * quantity) / maxPrintingSpeed;
+  buyToFlyRatio = volumeOfMachine / volume / quantity;
+  CO2eqForProcessing =
+    powerStandardOfMachine * processingTime * countryEnergyMix;
+  CO2eqForWaste =
+    (1 - reusablePortion) *
+    (buyToFlyRatio - 1) *
+    massOfPart *
+    (1 - recycledPortion) * emissionFactor;
+  let result = CO2eqForProcessing + CO2eqForWaste;
+  return result;
 };
