4d2af96fa88b774a28fbf73d7f3aa8acf0674296
Rajkumar Pambhar <rajkumarpambhar770@gmail.com>
2024-03-12 03:20:49 +0100

    Added button in Card component

--- siemens_sustech/src/app/components/card.js
+++ siemens_sustech/src/app/components/card.js
@@ -3,9 +3,9 @@ import React from "react";
 
 const Card = ({ companyName, companyLocation, result }) => {
   return (
-    <div className="bg-transparent border-white border rounded-md flex flex-col  min-w-72 hover:bg-slate-800">
+    <div className="bg-transparent border- border rounded-md flex flex-col">
       <div className="flex flex-row justify-end">
-        <span className="w-16 text-center text-white bg-red-500 rounded-tr">
+        <span className="w-16 text-center text-white bg-orange-600-500 rounded-tr bg-red-600">
           ${result}
         </span>
       </div>
@@ -21,6 +21,11 @@ const Card = ({ companyName, companyLocation, result }) => {
       <div className="flex flex-col gap-1/2 mx-5 my-5">
         <h1 className="text-white text-xl">{companyName}</h1>
         <h2 className="text-gray-400 text-m">{companyLocation}</h2>
+      </div>
+      <div className="flex items-center justify-center m-5">
+        <button className="text-white bg-transparent rounded border-2 border-[#009999] p-4 hover:bg-slate-800">
+          View Offer Details & Order
+        </button>
       </div>
     </div>
   );
